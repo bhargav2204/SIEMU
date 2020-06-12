152063265756517cd495f2666a1132c666707563
bhargav2204 <bhargavdhameliya007@gmail.com>
2020-06-12 22:41:50 +0530

    re design categorypage and googlesheet add

--- src/sections/Siemu/index.js
+++ src/sections/Siemu/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Card } from "react-rainbow-components";
+import { Card, CarouselImage, CarouselCard } from "react-rainbow-components";
+
+const carouselContainerStyles = {
+  maxWidth: 280,
+  height: 205,
+};
 export default function index() {
   return (
     <div className="bg-image">
@@ -12,7 +17,7 @@ export default function index() {
           <div className="col-md-3 col-xs-12">
             <div className=" rainbow-m-bottom_xsmall">
               <Link to="/siemu/productlist">
-                <Card>
+                {/* <Card>
                   <img
                     src="../../assets/images/product1.jpg"
                     className=""
@@ -28,14 +33,46 @@ export default function index() {
                   >
                     <h2>Consumer Product</h2>
                   </div>
-                </Card>
+                </Card> */}
+                <CarouselCard
+                  className="rainbow-m_auto"
+                  style={carouselContainerStyles}
+                >
+                  <CarouselImage
+                    src="../../assets/images/product1.jpg"
+                    style={{
+                      width: "-webkit-fill-available",
+                      height: " 169px",
+                    }}
+                    header="Consumer Product"
+                    alternativeText="First card accessible description."
+                  />
+                  <CarouselImage
+                    src="../../assets/images/cm1.jpg"
+                    style={{
+                      width: "-webkit-fill-available",
+                      height: " 169px",
+                    }}
+                    header="Consumer Product"
+                    alternativeText="Second card accessible description."
+                  />
+                  <CarouselImage
+                    src="../../assets/images/cm2.jpg"
+                    style={{
+                      width: "-webkit-fill-available",
+                      height: " 169px",
+                    }}
+                    header="Consumer Product"
+                    alternativeText="Third card accessible description."
+                  />
+                </CarouselCard>
               </Link>
             </div>
           </div>
           <div className="col-md-3 col-xs-12">
             <div className=" rainbow-m-bottom_xsmall">
               <Link to="/siemu/productlist">
-                <Card>
+                {/* <Card>
                   <img
                     src="../../assets/images/product2.jpg"
                     className=""
@@ -51,14 +88,46 @@ export default function index() {
                   >
                     <h2>Durable Products</h2>
                   </div>
-                </Card>
+                </Card> */}
+                <CarouselCard
+                  className="rainbow-m_auto"
+                  style={carouselContainerStyles}
+                >
+                  <CarouselImage
+                    src="../../assets/images/product2.jpg"
+                    style={{
+                      width: "-webkit-fill-available",
+                      height: " 169px",
+                    }}
+                    header="Durable Products"
+                    alternativeText="First card accessible description."
+                  />
+                  <CarouselImage
+                    src="../../assets/images/d1.jpg"
+                    style={{
+                      width: "-webkit-fill-available",
+                      height: " 169px",
+                    }}
+                    header="Durable Products"
+                    alternativeText="Second card accessible description."
+                  />
+                  <CarouselImage
+                    src="../../assets/images/d2.jpg"
+                    style={{
+                      width: "-webkit-fill-available",
+                      height: " 169px",
+                    }}
+                    header="Durable Products"
+                    alternativeText="Third card accessible description."
+                  />
+                </CarouselCard>
               </Link>
             </div>
           </div>
           <div className="col-md-3 col-xs-12">
             <div className=" rainbow-m-bottom_xsmall">
               <Link to="/siemu/productlist">
-                <Card>
+                {/* <Card>
                   <img
                     src="../../assets/images/petro.jpg"
                     className=""
@@ -74,14 +143,46 @@ export default function index() {
                   >
                     <h2>Petrluem Product</h2>
                   </div>
-                </Card>
+                </Card> */}
+                <CarouselCard
+                  className="rainbow-m_auto"
+                  style={carouselContainerStyles}
+                >
+                  <CarouselImage
+                    src="../../assets/images/petro.jpg"
+                    style={{
+                      width: "-webkit-fill-available",
+                      height: " 169px",
+                    }}
+                    header="Petrluem Product"
+                    alternativeText="First card accessible description."
+                  />
+                  <CarouselImage
+                    src="../../assets/images/p1.jpg"
+                    header="Petrluem Product"
+                    style={{
+                      width: "-webkit-fill-available",
+                      height: " 169px",
+                    }}
+                    alternativeText="Second card accessible description."
+                  />
+                  <CarouselImage
+                    src="../../assets/images/product3.jpg"
+                    style={{
+                      width: "-webkit-fill-available",
+                      height: " 169px",
+                    }}
+                    header="Petrluem Product"
+                    alternativeText="Third card accessible description."
+                  />
+                </CarouselCard>
               </Link>
             </div>
           </div>
           <div className="col-md-3 col-xs-12">
             <div className=" rainbow-m-bottom_xsmall">
               <Link to="/siemu/productlist">
-                <Card>
+                {/* <Card>
                   <img
                     src="../../assets/images/product4.jpg"
                     className=""
@@ -97,7 +198,39 @@ export default function index() {
                   >
                     <h2>Siemu Transaport</h2>
                   </div>
-                </Card>
+                </Card> */}
+                <CarouselCard
+                  className="rainbow-m_auto"
+                  style={carouselContainerStyles}
+                >
+                  <CarouselImage
+                    src="../../assets/images/product4.jpg"
+                    style={{
+                      width: "-webkit-fill-available",
+                      height: " 169px",
+                    }}
+                    header="Siemu Transaport"
+                    alternativeText="First card accessible description."
+                  />
+                  <CarouselImage
+                    src="../../assets/images/t1.jpg"
+                    style={{
+                      width: "-webkit-fill-available",
+                      height: " 169px",
+                    }}
+                    header="Siemu Transaport"
+                    alternativeText="Second card accessible description."
+                  />
+                  <CarouselImage
+                    src="../../assets/images/t2.jpg"
+                    style={{
+                      width: "-webkit-fill-available",
+                      height: " 169px",
+                    }}
+                    header="Siemu Transaport"
+                    alternativeText="Third card accessible description."
+                  />
+                </CarouselCard>
               </Link>
             </div>
           </div>
@@ -106,7 +239,7 @@ export default function index() {
           <div className="col-md-3 col-xs-12">
             <div className=" rainbow-m-bottom_xsmall">
               <Link to="/siemu/productlist">
-                <Card>
+                {/* <Card>
                   <img
                     src="../../assets/images/product5.jpg"
                     className=""
@@ -122,14 +255,46 @@ export default function index() {
                   >
                     <h2>Industrial Product</h2>
                   </div>
-                </Card>
+                </Card> */}
+                <CarouselCard
+                  className="rainbow-m_auto"
+                  style={carouselContainerStyles}
+                >
+                  <CarouselImage
+                    src="../../assets/images/product5.jpg"
+                    style={{
+                      width: "-webkit-fill-available",
+                      height: " 169px",
+                    }}
+                    header="Industrial Product"
+                    alternativeText="First card accessible description."
+                  />
+                  <CarouselImage
+                    src="../../assets/images/i1.jpg"
+                    style={{
+                      width: "-webkit-fill-available",
+                      height: " 169px",
+                    }}
+                    header="Industrial Product"
+                    alternativeText="Second card accessible description."
+                  />
+                  <CarouselImage
+                    src="../../assets/images/i2.jpg"
+                    style={{
+                      width: "-webkit-fill-available",
+                      height: " 169px",
+                    }}
+                    header="Industrial Product"
+                    alternativeText="Third card accessible description."
+                  />
+                </CarouselCard>
               </Link>
             </div>
           </div>
           <div className="col-md-3 col-xs-12">
             <div className=" rainbow-m-bottom_xsmall">
               <Link to="/siemu/productlist">
-                <Card>
+                {/* <Card>
                   <img
                     src="../../assets/images/hyg.jpg"
                     className=""
@@ -145,14 +310,46 @@ export default function index() {
                   >
                     <h2>Health & Hygine</h2>
                   </div>
-                </Card>
+                </Card> */}
+                <CarouselCard
+                  className="rainbow-m_auto"
+                  style={carouselContainerStyles}
+                >
+                  <CarouselImage
+                    src="../../assets/images/hyg.jpg"
+                    style={{
+                      width: "-webkit-fill-available",
+                      height: " 169px",
+                    }}
+                    header="Health & Hygine"
+                    alternativeText="First card accessible description."
+                  />
+                  <CarouselImage
+                    src="../../assets/images/h1.jpg"
+                    style={{
+                      width: "-webkit-fill-available",
+                      height: " 169px",
+                    }}
+                    header="Health & Hygine"
+                    alternativeText="Second card accessible description."
+                  />
+                  <CarouselImage
+                    src="../../assets/images/h2.jpg"
+                    style={{
+                      width: "-webkit-fill-available",
+                      height: " 169px",
+                    }}
+                    header="Health & Hygine"
+                    alternativeText="Third card accessible description."
+                  />
+                </CarouselCard>
               </Link>
             </div>
           </div>
           <div className="col-md-3 col-xs-12">
             <div className=" rainbow-m-bottom_xsmall">
               <Link to="/siemu/productlist">
-                <Card>
+                {/* <Card>
                   <img
                     src="../../assets/images/good.jpg"
                     className=""
@@ -168,14 +365,46 @@ export default function index() {
                   >
                     <h2>Compliment Goods</h2>
                   </div>
-                </Card>
+                </Card> */}
+                <CarouselCard
+                  className="rainbow-m_auto"
+                  style={carouselContainerStyles}
+                >
+                  <CarouselImage
+                    src="../../assets/images/good.jpg"
+                    style={{
+                      width: "-webkit-fill-available",
+                      height: " 169px",
+                    }}
+                    header="Compliment Goods"
+                    alternativeText="First card accessible description."
+                  />
+                  <CarouselImage
+                    src="../../assets/images/product7.jpg"
+                    style={{
+                      width: "-webkit-fill-available",
+                      height: " 169px",
+                    }}
+                    header="Compliment Goods"
+                    alternativeText="Second card accessible description."
+                  />
+                  <CarouselImage
+                    src="../../assets/images/good.jpg"
+                    style={{
+                      width: "-webkit-fill-available",
+                      height: " 169px",
+                    }}
+                    header="Compliment Goods"
+                    alternativeText="Third card accessible description."
+                  />
+                </CarouselCard>
               </Link>
             </div>
           </div>
           <div className="col-md-3 col-xs-12">
             <div className=" rainbow-m-bottom_xsmall">
               <Link to="/siemu/productlist">
-                <Card>
+                {/* <Card>
                   <img
                     src="../../assets/images/luxry.jpeg"
                     className=""
@@ -191,7 +420,39 @@ export default function index() {
                   >
                     <h2>Luxury Goods</h2>
                   </div>
-                </Card>
+                </Card> */}
+                <CarouselCard
+                  className="rainbow-m_auto"
+                  style={carouselContainerStyles}
+                >
+                  <CarouselImage
+                    src="../../assets/images/luxry.jpeg"
+                    style={{
+                      width: "-webkit-fill-available",
+                      height: " 169px",
+                    }}
+                    header="Luxury Goods"
+                    alternativeText="First card accessible description."
+                  />
+                  <CarouselImage
+                    src="../../assets/images/l1.jpg"
+                    style={{
+                      width: "-webkit-fill-available",
+                      height: " 169px",
+                    }}
+                    header="Luxury Goods"
+                    alternativeText="Second card accessible description."
+                  />
+                  <CarouselImage
+                    src="../../assets/images/l2.jpg"
+                    style={{
+                      width: "-webkit-fill-available",
+                      height: " 169px",
+                    }}
+                    header="Luxury Goods"
+                    alternativeText="Third card accessible description."
+                  />
+                </CarouselCard>
               </Link>
             </div>
           </div>
