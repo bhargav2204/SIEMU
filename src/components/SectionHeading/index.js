e4433ae860aa0bae2915efb09c60f7f2035610ce
rohitkothiya <rohit.kothiya2012@gmail.com>
2020-07-01 00:00:08 +0530

    Logo link added

--- src/components/SectionHeading/index.js
+++ src/components/SectionHeading/index.js
@@ -9,17 +9,23 @@ import "./styles.css";
 import { Link } from "react-router-dom";
 
 export default function SectionHeading({ onToogleSidebar }) {
+
+  const handleRedirectToHomePage =()=> {
+    console.log("called")
+  }
   return (
     <header
       className="react-rainbow-admin_header rainbow-position_fixed rainbow-flex rainbow-align_center rainbow-p-horizontal_large rainbow-background-color_white"
       style={{ background: "rgb(2, 65, 142)", height: "50px" }}
     >
+    <Link to="/siemu">
       <img
+      onClick={handleRedirectToHomePage}
         src={require("../../Asset/headerlogo.jpeg")}
         alt="rainbow logo"
         className="react-rainbow-admin_header-logo"
       />
-
+</Link>
       <div className="heading">{/* SIEMU */}</div>
 
       <section className="rainbow-flex rainbow-align_center react-rainbow-admin_header-actions" />
