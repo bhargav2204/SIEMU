c38a7ff7382edd9946a588deea6a55306df70167
bhargav2204 <bhargavdhameliya007@gmail.com>
2020-07-01 00:26:31 +0530

    s

--- src/history.js
+++ src/history.js
@@ -1,7 +1,7 @@
-import createBrowserHistory from 'history/createBrowserHistory';
+import createBrowserHistory from "history/createBrowserHistory";
 
 const instance = createBrowserHistory();
 
-export const navigateTo = path => instance.push(path);
+export const navigateTo = (path) => instance.push(path);
 
 export default instance;
